634e9a6b539370d8130b8ba90b3894124c724ed9
JuanBueno01 <juanpablobuenolopez@gmail.com>
2025-01-25 19:58:14 -0500

    movimiento_seba

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   public GameObject playerObj;
+  public Rigidbody rb;
+  public float moveSpeed;
+  public bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
@@ -15,8 +18,16 @@ public class PlayerController : MonoBehaviour
     {
      float xInput = Input.GetAxisRaw("Horizontal");
      float yInput = Input.GetAxisRaw("Vertical");
+      if (isGrounded){
+        playerObj.transform.Translate(Vector3.up * yInput * Time.deltaTime);
+        rb.AddRelativeForce(Vector3.right * xInput * Time.deltaTime * moveSpeed);
+      }
+     
       
-     playerObj.transform.Translate(Vector3.up * yInput * Time.deltaTime);
+      //if (Input.GetButtonDown("Jump") == true)
+      //{
+        //Debug.Log("hi");
+      //}
 
      
      
@@ -26,5 +37,15 @@ public class PlayerController : MonoBehaviour
       //{
         //Debug.Log("hi");
       //}   
+    }
+    void OnCollisionEnter(Collision collision){
+      if (collision.gameObject.tag == "Ground"){
+        isGrounded = true;
+      }
+    }
+    void OnCollisionExit(Collision collision){
+      if (collision.gameObject.tag == "Ground"){
+        isGrounded = true;
+      }
     }
 }
